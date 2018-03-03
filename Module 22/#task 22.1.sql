b613af8b5660b97c959557d81c9b2311d65b81ed
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2018-03-03 23:55:36 +0100

    Module and Task 22.1 - v1.0.1

--- Module 22/#task 22.1.sql
+++ Module 22/#task 22.1.sql
@@ -8,7 +8,8 @@ begin
 	declare result varchar(255) default 'Wrong id were used';
     
     if findid > 0 then
-		set result = (concat( (select firstname from readers where reader_id = findid), ' ', (select lastname from readers where reader_id = findid)));
+		#set result = (concat( (select firstname from readers where reader_id = findid), ' ', (select lastname from readers where reader_id = findid)));
+        set result = (select concat(firstname, ' ', lastname) as name from readers where reader_id = findid);
     end if;
     
     
